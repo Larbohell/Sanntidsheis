e8a3e00d53e1cf6d25024674c70c89fae0be02c4
bendikiv <bendikiv@gmail.com>
2016-01-19 13:51:20 +0100

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,33 @@
+# Python 3.3.3 and 2.7.6
+# python helloworld_python.py
+
+from threading import Thread
+
+i = 0
+
+def thread_1_func():
+    	global i
+	
+	for j in range(0,1000000):
+		i+=1 
+
+def thread_2_func():
+    	global i
+	
+	for j in range(0,1000000):
+		i-=1
+	
+
+def main():
+	thread_1 = Thread(target = thread_1_func, args = ())
+	thread_2 = Thread(target = thread_2_func, args = ())
+	thread_1.start()
+	thread_2.start()
+
+	thread_1.join()
+	thread_2.join()
+
+	print i
+
+
+main()
