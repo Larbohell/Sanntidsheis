17a66c3574b2eaa7d77bc7920905f44cee463ce6
Kari Engen Guttulsrud <eparis@atlassian.com>
2016-01-20 10:24:25 +0100

    added semaphores in c

--- exercise1.c
+++ exercise1.c
@@ -3,25 +3,36 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <semaphore.h>
 
 int i = 0;
+sem_t sem;
 
 void* thread_1_func(){
 	for (int j = 0; j<1000000; j++){
+		sem_wait(&sem);
 		i++;
+		sem_post(&sem);
 	}
 	return NULL;
 }
 
 void* thread_2_func(){
+	
 	for (int j = 0; j<1000000; j++){
+		sem_wait(&sem);
 		i--;
+		sem_post(&sem);
 	}
+	
 	return NULL;
 }
 
 
 int main(){
+
+	
+	sem_init(&sem, 0, 1);
 
 	pthread_t thread_1;
 	pthread_create(&thread_1, NULL, thread_1_func, &i);
