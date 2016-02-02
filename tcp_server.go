ec96d2e7b024cdc2e8ec559ae3dca1f855c92d1f
Student <student@itk-d650>
2016-02-02 13:07:07 +0100

    Added tcp_server.go, which is abel to connect to tcp_client.go

--- tcp_server.go
+++ tcp_server.go
@@ -2,30 +2,56 @@ package main
 
 import "net"
 import "fmt"
-import "bufio"
+//import "bytes"
 //import "strings"
+import "unicode/utf8"
 
 
+const (
+    CONN_HOST = "129.241.187.161"	// For work station 8
+    CONN_PORT = "34933"	// For fixed-length messages
+    CONN_TYPE = "tcp"
+)
 
-func main() {
-	fmt.Print("1\n")
-
-	listener, _:= net.Listen("tcp", "127.0.0.1:33546")
-	//if (error){}
-	fmt.Print("2\n")
-
-	connection, _:= listener.Accept()
-	//if (error){}
-	fmt.Print("3\n")
-
-	for {
-		fmt.Print("new for\n")
-		message, _ := bufio.NewReader(connection).ReadString('\n')
-
-		fmt.Print("Message Received:", string(message))
-
-	}
+func CheckError(err error) {
+    if err  != nil {
+        fmt.Println("Error: " , err)
+    }
 }
 
-//uint8 [1024] buffer
-//uint32 fromWho
+func main() {
+
+	serverListener, err := net.Listen("tcp", CONN_HOST+":"+CONN_PORT)
+	CheckError(err)
+
+	serverConnection, err := serverListener.Accept();
+	CheckError(err)
+
+	//welcome_message := []byte("Welcome to this server.")
+	welcome_message := []byte("Welcome to this server.")
+	_, err = serverConnection.Write(welcome_message)
+	CheckError(err)
+
+	for {
+		_ = handleRequest(serverConnection)
+	}
+
+}
+
+func handleRequest(connection net.Conn) (int){	
+	incoming_data := make([]byte, 1024)
+	
+	received_message_length, err := connection.Read(incoming_data)
+	CheckError(err)
+
+	received_message := string(incoming_data[:received_message_length - 1])
+
+	fmt.Println("Incoming message: ", received_message)
+
+	message := []byte("Message received.")
+
+	_, err = connection.Write(message)
+	CheckError(err)
+	
+	return 1
+}
